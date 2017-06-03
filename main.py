6ee20700cfb4deeede3a51908e17176bb792e116
Felix Gifford <gifford.felix@gmail.com>
2017-06-04 05:04:13 +1000

    Added strain diffractometry

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #!/Users/felixgifford/anaconda/bin/python3
+print("Importing Modules")
 import sys, os
+print("System libraries")
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
+print("Numpy")
+import scipy.stats
+print("Scipy Statistics")
+#import matplotlib as mpl
+print("Matplotlib")
+#import matplotlib.pyplot as plt
+print("Pyplot")
 #mpl.rc('text', usetex = True)
 #mpl.rc('font', **{'family':"sans-serif"})
 #params = {'text.latex.preamble': [r'\usepackage{siunitx}']}   
@@ -13,18 +19,32 @@ width = 12.5
 height = 5
 area = width*height
 
-data_path = input("Path to data: ")
-cross_section = float(input("Cross sectional area of Sample: "))
-poisson = float(input("Enter Poisson ratio: "))
+data_path = "data/stress_strain_6061.txt" #input("Path to data: ")
+cross_section = area #float(input("Cross sectional area of Sample: "))
+poisson = 0.3 #float(input("Enter Poisson ratio: "))
 
-data = np.loadtxt(data_path, skiprows=1)
+raw_data = np.loadtxt(data_path, skiprows=1)
 
 #order data by x axis (col 0)
-#convert data to stress and strain
-#take elastic modulus measurement
+sorted_data = raw_data[raw_data[:,0].argsort()]
 
-plt.plot(data[:,0],data[:,1], linestyle="-", color='r')
-print(data)
+#convert data to stress and strain
+# data: [L+delta_0, P_0]
+#		[L+delta_0, P_0] * [1/cross_section]
+#		[........., ...]   [(delta_n-delta_1)/delta_n] <----
+#		[L+delta_n, P_n]
+
+d = (sorted_data * [1,1/cross_section])
+d[:,0] = ((d[:,0]-d[0,0])/d[:,0])
+
+#take elastic modulus measurement
+elastic_modulus,i,r,p,err = scipy.stats.linregress(d[0:int(len(d)*0.2)])
+print(s)
+#elastic_modulus = 
+#error = 
+
+'''plt.plot(d[:,0],d[:,1], linestyle="-", color='r')
+#print(data)
 
 
 plt.ylabel(r"$\sigma MPa$")
@@ -35,4 +55,4 @@ plt.axis('auto')
 #            arrowprops=dict(facecolor='black', shrink=0.05),
 #            )
 
-plt.show()
+plt.show()'''
